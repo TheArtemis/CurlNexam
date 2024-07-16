10dbac1c4a6887987307d4f13844ca58a6a26dd7
TheArtemis <lordef01@gmail.com>
2024-07-16 22:06:11 +0200

    Now you can pass a custom port

--- AZ-900/start.py
+++ AZ-900/start.py
@@ -1,20 +1,28 @@
 import http.server
 import socketserver
 import os
+import argparse
 
-PORT = 1234
+DEFAULT_PORT = 1234
 DIRECTORY = "pages"
 
 class CustomHandler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=DIRECTORY, **kwargs)
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run a simple HTTP server.")
+    parser.add_argument("-p", "--port", type=int, default=DEFAULT_PORT, help="Specify port number, default is 1234.")
+    return parser.parse_args()       
 
-def run_server(port=PORT):
+
+def run_server(port=DEFAULT_PORT):
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
     with socketserver.TCPServer(("", port), CustomHandler) as httpd:
         print(f"Serving pages at http://localhost:{port}")
         httpd.serve_forever()
 
-run_server()
+if __name__ == "__main__":
+    args = parse_args()
+    run_server(port=args.port)
